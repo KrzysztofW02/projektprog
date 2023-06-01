5c781998a85cdff5ecf391ae20b9a86bcc4aff80
KrzysztofW02 <98061276+KrzysztofW02@users.noreply.github.com>
2023-06-01 17:15:52 +0200

    Add files via upload

---
+++ Aplikacja do treningu/ViewModels/MainViewModels.cs
@@ -0,0 +1,28 @@
+﻿using Aplikacja_do_treningu.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+namespace Aplikacja_do_treningu.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand OpenSilowyCommand { get; }
+
+        public MainViewModel()
+        {
+            OpenSilowyCommand = new RelayCommand(OpenSilowyWindow);
+        }
+
+        private void OpenSilowyWindow()
+        {
+            SilowyView silowyWindow = new SilowyView();
+            silowyWindow.Show();
+        }
+    }
+}
---
+++ Aplikacja do treningu/Views/NormalnyView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Aplikacja_do_treningu.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikacja_do_treningu.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Normalny.xaml
+    /// </summary>
+    public partial class Normalny : Window
+    {
+        public Normalny()
+        {
+            InitializeComponent();
+            DataContext = new NormalnyViewModels();
+        }
+    }
+}
